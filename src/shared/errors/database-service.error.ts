ba3c06d2180ab436a06d477fea49dfc89d177c1b
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-30 17:55:59 -0300

    feat: add `DatabaseServiceError` and change error catching in `database.ts`

---
+++ src/shared/errors/database-service.error.ts
@@ -0,0 +1,24 @@
+import { CustomError, ErrorCodes } from "@/shared/errors/custom-errors";
+import { PG_ERROR_CODES, SingleKeyViolationCode } from "@/features/providers/database";
+
+export type DatabaseErrorInfo = {
+  code: string;
+  constraint?: string;
+  detail?: string;
+};
+
+export class DatabaseServiceError extends CustomError {
+  constructor(message: string, info: DatabaseErrorInfo) {
+    let details;
+    let errorCode = ErrorCodes.Unknown;
+    if (info.detail && info.constraint) details = [{ key: info.constraint, message: info.detail }];
+    if (PG_ERROR_CODES.includes(info.code)) errorCode = ErrorCodes.InvalidDatabaseTransactionData;
+    if (info.code === SingleKeyViolationCode) errorCode = ErrorCodes.ResourceAlreadyExists;
+
+    super(
+      `There was an error in the database operation with code ${info.code} and message: ${message}.`,
+      errorCode,
+      details,
+    );
+  }
+}
