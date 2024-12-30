7f7ff28134e0ec9827546924d6ec21a80e2feb73
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-30 17:57:28 -0300

    test: change value in `toBe` for `HttpCodes`

--- __tests__/integration/v1/health/get.test.ts
+++ __tests__/integration/v1/health/get.test.ts
@@ -1,4 +1,5 @@
 import { BaseUrl, waitForAllServices } from "../../../orchestrator";
+import { HttpCodes } from "../../../../src/shared/http/http";
 
 beforeAll(async () => await waitForAllServices());
 
@@ -6,7 +7,7 @@ describe("GET /api/v1/health", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system health", async () => {
       const response = await fetch(`${BaseUrl}/v1/health`);
-      expect(response.status).toBe(200);
+      expect(response.status).toBe(HttpCodes.OK);
       const responseJson = await response.json();
       expect(responseJson).toEqual({
         updated_at: new Date(responseJson.updated_at).toISOString(),
